47579c31cf01b7d50230dd2d8e6c51c110153e37
BakhtiarAhmed41 <ahmedbakhtiar41@gmail.com>
2024-01-06 00:03:19 +0500

    Home page created with route to login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,30 @@
+import { Poppins } from 'next/font/google'
 import {Button} from '@/components/ui/button'
+import{cn} from  '@/lib/utils'
+import { LoginButton } from '@/components/auth/login_button'
 
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["600"]
+})
 export default function Home() {
   return (
-   <div className='mt-10 text-center'> <h1 className='font-bold text-center text-3xl	 font- text-blue-600 mb-10'> Implementing NextAuth</h1>
-    <Button size="lg" variant="outline"> Click Me </Button>
+   
+   <main className='flex h-full flex-col items-center justify-center bg-sky-600'>
+    <div className='space-y-6 text-center'>
+      <h1 className={cn('text-6xl font-bold text-white drop-shadow-md', font.className,) }>
+          🔐 Auth
+      </h1>
+      <p className='text-white text-lg'>
+        A simple authentication service
+      </p>
+      <LoginButton>
+      <Button variant="secondary" size="lg" className='mt-5'>
+        Sign in 
+      </Button>
+      </LoginButton>
     </div>
+
+   </main>
     )
 }
